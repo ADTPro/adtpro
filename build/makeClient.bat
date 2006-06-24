3ec271256866c4c59cef216ee7e3543e4e7ddb4c
David Schmidt <david__schmidt@users.sourceforge.net>
2006-06-24 13:24:27 +0000

    Build process tweaks, version bump

--- build/makeClient.bat
+++ build/makeClient.bat
@@ -10,11 +10,13 @@
 @rem Parameters:
 @rem   diskname - the name of the Apple ][ disk to modify
 @rem
-@cd ..\client\src
+@rem Need to know where sbasm is...
+@rem
+@path=%PATH%;C:\dev\xassm\sbasm\2.06
 @call sbasm main.asm 2> ..\..\build\asm.out
 @copy adtpro ..\..\build
 @cd ..\..\build
 @type asm.out | grep "in assembly"
 @copy /y ADTProBase.dsk %1.dsk
-@call ac -d adtpro %1.dsk
+@rem @call ac -d adtpro %1.dsk
 @type adtpro | ac -p adtpro bin 2051 %1.dsk
