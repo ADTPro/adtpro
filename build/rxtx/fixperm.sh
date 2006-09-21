7dfd99a39b965f4c96f432ac2c466ff1dfda67ef
David Schmidt <david__schmidt@users.sourceforge.net>
2006-09-21 14:47:42 +0000

    Updates for OSX install...

--- build/rxtx/fixperm.sh
+++ build/rxtx/fixperm.sh
@@ -1,18 +1,10 @@
 #!/bin/sh 
-
-#
-# Run this on OS X macines.  Bring up the Terminal and type:
-# sh fixperm.sh
-#
-
 curruser=`sudo id -p | grep 'login' | sed 's/login.//'` 
 echo $curruser 
- 
 if [ ! -d /var/lock ] 
 then 
 sudo mkdir /var/lock 
 fi 
- 
 sudo chgrp uucp /var/lock 
 sudo chmod 775 /var/lock 
 if [ ! `sudo niutil -readprop / /groups/uucp users | grep $curruser  
