22cb3dbc849a0275a29f37cd5a3b33b0b3074000
David Schmidt <david__schmidt@users.sourceforge.net>
2007-05-15 20:50:55 +0000

    *nix invocation updates

--- build/adtprobase.sh
+++ build/adtprobase.sh
@@ -11,4 +11,15 @@
 # export MY_JAVA_HOME=/usr/local/java/bin/ (need final slash)
 # export ADTPRO_HOME=~/myuser/adtpro/  (need final slash)
 #
-"$MY_JAVA_HOME"java -Xms256m -jar "$ADTPRO_HOME"%ADTPRO_VERSION% &
+# For Linux, uncomment this:
+#  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$ADTPRO_HOME"rxtx/Linux/i686-unknown-linux-gnu
+#
+# For OSX, uncomment this:
+   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$ADTPRO_HOME"rxtx/Mac_OS_X
+# (Also remember to run fixperm.sh in the rxtx directory for OSX...)
+#
+# For Solaris, uncomment this:
+#  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$ADTPRO_HOME"rxtx/Solaris/sparc-solaris/sparc32-sun-solaris2.8
+#
+# Execute the thing:
+"$MY_JAVA_HOME"java -Xms256m -cp "$ADTPRO_HOME"%ADTPRO_VERSION%:"$ADTPRO_HOME"rxtx/RXTXcomm.jar org.adtpro.ADTPro &
