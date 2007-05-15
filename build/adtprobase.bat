dd7d5cd79f89235a183e7a9c1f2b21221a35b351
David Schmidt <david__schmidt@users.sourceforge.net>
2007-05-15 20:59:30 +0000

    Make installation a little less painful

--- build/adtprobase.bat
+++ build/adtprobase.bat
@@ -11,4 +11,5 @@
 @REM SET ADTPRO_HOME=C:\src\workspace\311\adtpro\build\  (need final backslash)
 @REM SET MY_JAVA_HOME=C:\Progra~1\IBM\Java142\bin\ (need final backslash)
 @REM
-@start /min %MY_JAVA_HOME%java -Xms128m -cp %ADTPRO_HOME%%ADTPRO_VERSION%;%ADTPRO_HOME%lib\RXTXcomm.jar org.adtpro.ADTPro
+@PATH=%PATH%;%ADTPRO_HOME%rxtx\Windows\i368-mingw32;
+@start /min %MY_JAVA_HOME%java -Xms128m -cp %ADTPRO_HOME%%ADTPRO_VERSION%;%ADTPRO_HOME%rxtx\RXTXcomm.jar org.adtpro.ADTPro
