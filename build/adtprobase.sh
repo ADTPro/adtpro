b69716db4a3eb7bdfc2360a2ec7f67f391acbb0d
David Schmidt <david__schmidt@users.sourceforge.net>
2007-05-16 12:29:13 +0000

    *nix invocation updates

--- build/adtprobase.sh
+++ build/adtprobase.sh
@@ -11,19 +11,21 @@
 # export MY_JAVA_HOME=/usr/local/java/bin/ (need final slash)
 # export ADTPRO_HOME=~/myuser/adtpro/  (need final slash)
 #
-# For Linux, uncomment this:
-#  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$ADTPRO_HOME"rxtx/Linux/i686-unknown-linux-gnu
+# For Linux, uncomment this line:
+#  export RXTXLIB=rxtx/Linux/i686-unknown-linux-gnu
 #
-# For OSX, uncomment these three lines:
-   export TWEAK1="-Djava.library.path="
-   export TWEAK2="rxtx/Mac_OS_X"
-   export TWEAK=$TWEAK1$ADTPRO_HOME$TWEAK2
+# For OSX, uncomment this line:
+   export RXTXLIB=rxtx/Mac_OS_X
 # (Also remember to run fixperm.sh in the rxtx directory once on OSX...)
 #
 # For Solaris, uncomment this:
-#  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$ADTPRO_HOME"rxtx/Solaris/sparc-solaris/sparc32-sun-solaris2.8
+#  export RXTXLIB=rxtx/Solaris/sparc-solaris/sparc32-sun-solaris2.8
 #
-# Execute the thing.
+# Set up the library location.
+export TWEAK1="-Djava.library.path="
+export TWEAK=$TWEAK1$ADTPRO_HOME$RXTXLIB
+#
+# Set up a comfortable Java execution environment.
 # We want to execute Java (1), set a larger-than-default heap size (2), tell the OS where to find a 
 # native library to support rxtx (3), set the classpath to include ADTPro (4) and RXTXcomm (5), 
 # tell Java what the class to execute is, then finally put it all in the background.  To wit:
