92b875add3af4958258177f7f6e9ed2ce9a30644
David Schmidt <david__schmidt@users.sourceforge.net>
2007-06-03 17:41:29 +0000

    Build changes - shove everything down one level in the distribution packages

--- client/src/print.asm
+++ client/src/print.asm
@@ -524,7 +524,7 @@ MSGTBL:
 	.addr MUTHBAD, MPREFIX, MINSERTDISK
 	.addr MNULL
 
-MSG01:	ascz "1.0.0"
+MSG01:	ascz "v.r.m"
 MSG02:	asccr "(S)END (R)ECEIVE (D)IR (B)ATCH (C)D"
 	.byte $8d,$00
 MSG03:	ascz "(V)OLUMES CONFI(G) (?)ABOUT (Q)UIT:"
