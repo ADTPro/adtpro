3f01f7c25d2743c8f0bc15c53671cb062e1055c6
David Schmidt <david__schmidt@users.sourceforge.net>
2007-06-19 13:59:10 +0000

    Shell script improvements - better cwd determinations

--- build/adtprobase.sh
+++ build/adtprobase.sh
@@ -6,17 +6,20 @@
 #   1. $MY_JAVA_HOME - to pick a particular java to run under
 #   2. $ADTPRO_HOME - to say where you installed ADTPro
 #
-# Setting default ADTPRO_HOME to be the current working directory.
-export ADTPRO_HOME="`dirname \"$0\"`/"
+# Set default ADTPRO_HOME to be the fully qualified
+# current working directory.
+export ADTPRO_HOME="`dirname \"$0\"`"
+cd $ADTPRO_HOME
+export ADTPRO_HOME=`pwd`
 
-# Uncomment and modify one or both of the lines below.
-# NOTE: you need a trailing slash.
+# Uncomment and modify one or both of the lines below if you
+# want to specify a particular location for Java or ADTPro.
+# NOTE: be sure to include a trailing slash on MY_JAVA_HOME.
 #
 # export MY_JAVA_HOME=/usr/local/java/bin/
-# export ADTPRO_HOME=~/myuser/adtpro/
+# export ADTPRO_HOME=~/myuser/adtpro
 
 OS=`uname`
-CWD="`dirname \"$0\"`"
 
 # For Linux, use this:
 if [ "$OS" = "Linux" ]; then
@@ -27,7 +30,6 @@ fi
 # (Also remember to run fixperm.command in the rxtx directory once on OSX...)
 if [ "$OS" = "Darwin" ]; then
   export RXTXLIB=lib/rxtx/Mac_OS_X
-  cd $CWD
 fi
 
 # For Solaris, use this:
@@ -37,7 +39,7 @@ fi
 
 # Set up the library location.
 export TWEAK1="-Djava.library.path="
-export TWEAK=$TWEAK1$ADTPRO_HOME$RXTXLIB
+export TWEAK=$TWEAK1$ADTPRO_HOME/$RXTXLIB
 
 # Set up a comfortable Java execution environment.
 # We want to execute Java (1), set a larger-than-default heap size (2),
@@ -45,7 +47,6 @@ export TWEAK=$TWEAK1$ADTPRO_HOME$RXTXLIB
 # the classpath to include ADTPro (4) and RXTXcomm (5), and finally
 # tell Java what the class to execute is.  
 # To wit:
-cd "$ADTPRO_HOME"disks
+cd "$ADTPRO_HOME"/disks
 "$MY_JAVA_HOME"java -Xms256m -Xmx512m $TWEAK -cp ../lib/%ADTPRO_VERSION%:../lib/rxtx/RXTXcomm.jar org.adtpro.ADTPro
 #               (1)     (2)     (2)     (3)                              (4)                            (5)            (6)
-cd $CWD
