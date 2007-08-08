09495719534342de2427cb4c2dcfa784be7b41ed
David Schmidt <david__schmidt@users.sourceforge.net>
2007-08-08 16:43:05 +0000

    Doc directory maintenance

---
+++ doc/buildpost.bat
@@ -0,0 +1,23 @@
+cd target\site
+perl -i.orig -p ..\..\removeMaven.re batch.html
+perl -i.orig -p ..\..\removeMaven.re bootstrap.html
+perl -i.orig -p ..\..\removeMaven.re bootstrapaudio.html
+perl -i.orig -p ..\..\removeMaven.re bootstrapmain.html
+perl -i.orig -p ..\..\removeMaven.re configaudio.html
+perl -i.orig -p ..\..\removeMaven.re configaudiomain.html
+perl -i.orig -p ..\..\removeMaven.re configethernet.html
+perl -i.orig -p ..\..\removeMaven.re configserial.html
+perl -i.orig -p ..\..\removeMaven.re configserialmain.html
+perl -i.orig -p ..\..\removeMaven.re connectionsaudio.html
+perl -i.orig -p ..\..\removeMaven.re connectionsserial.html
+perl -i.orig -p ..\..\removeMaven.re credits.html
+perl -i.orig -p ..\..\removeMaven.re developing.html
+perl -i.orig -p ..\..\removeMaven.re history.html
+perl -i.orig -p ..\..\removeMaven.re index.html
+perl -i.orig -p ..\..\removeMaven.re install.html
+perl -i.orig -p ..\..\removeMaven.re misc.html
+perl -i.orig -p ..\..\removeMaven.re protocol.html
+perl -i.orig -p ..\..\removeMaven.re receiving.html
+perl -i.orig -p ..\..\removeMaven.re sending.html
+perl -i.orig -p ..\..\removeMaven.re starting.html
+cd ..\..
