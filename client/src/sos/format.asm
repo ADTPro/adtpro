b54bc703503632bcda2379e4be4362114a687e4c
David Schmidt <david__schmidt@users.sourceforge.net>
2008-09-01 13:57:31 +0000

    Wiring up some more formatting behavior for SOS

--- client/src/sos/format.asm
+++ client/src/sos/format.asm
@@ -33,6 +33,9 @@ FormatEntry:
 			;   NUMBLKS+1
 	bmi FormatAbort	; User wanted to abandon the format operation.
 
+	jsr LName		; Ask for a name
+	bcs Again
+
 	ldx #$18		; Check for a floppy and its formatter device driver
 	cpx NUMBLKS
 	bne HighLevelFormat	; If it's not $118 blocks, it's not a .FMTDx-able device
@@ -58,10 +61,24 @@ FormatEntry:
 	lda FMTDX,y		; Load the device number for the formatter associated with this drive
 	sta FMT_CONTROL_DEV_NUM
 	jsr FloppyLLFormat	; Ask the formatter to low-level format the floppy
-	bcs FormatAbortPause
+	bcs Again
 
 HighLevelFormat:
 	jsr PAUSE
+	rts
+
+; High-level format here
+	jsr HighLevelFormat
+
+Again:
+	lda #$17
+	jsr TABV
+	ldy #PMNuther
+	jsr WRITEMSGLEFT
+	jsr YNLOOP		; Get a Yes or No answer
+	beq MExit		; Answer was No...
+	jmp FormatEntry		; Format another disk
+MExit:
 	rts
 
 ; Some things that main.asm expects to see as part of the normal format:
@@ -124,7 +141,7 @@ FloppyLLFormat:
 	bne :+
 	jsr CROUT
 	ldax #FMTMsg02
-	ldy FMT_MSG_LEN_TBL
+	ldy FMT_MSG_LEN_TBL+1
 	jsr IPShowMsg
 	jmp FloppyLLFormatOk
 :	pha
@@ -139,10 +156,92 @@ FloppyLLFormatDead:
 FloppyLLFormatOk:
 	rts
 
+;*******************************
+;*                             *
+;* LName: Prompt for unit name *
+;*                             *
+;*******************************
+LName:
+	lda #$14
+	jsr TABV
+	ldy #PMVolName
+	jsr WRITEMSGLEFT
+LRdname:
+	lda #$0E		; Reset CH to 14
+	jsr HTAB
+	jsr ECHO_ON
+	ldx #$00
+	beq LInput		; Always taken
+LBackup:
+	cpx #0  		; Handle backspaces
+	beq LRdname
+	dex
+	lda #$88		; <--
+	jsr COUT
+LInput:
+	jsr RDKEY		; Get a keypress
+	cmp #$88		; Backspace?
+	beq LBackup
+	cmp #$FF		; Delete?
+	beq LBackup
+	cmp #$8D		; C/R is end of input
+	beq LFormat
+	cmp #CHR_ESC		; Escape bails out
+	beq LAbort
+	cmp #$AE		; (periods are ok...)
+	beq LStore
+	cmp #$B0		; Less than '0'?
+	bcc LInput		; Then it's invalid
+	cmp #$BA		; Less than '9'+1?
+	bcc LStore		; Then it's a number - store it
+
+	and #$DF		; Force any lower case to upper case
+	cmp #$C1		; Less than 'A'?
+	bcc LInput		; Then it's invalid
+	cmp #$DB		; Less than 'Z'+1?
+	bcc LStore		; Then it's a letter - store it
+
+	jmp LInput		; So sorry, try again
+LStore:
+	jsr COUT		; Print keypress on the screen
+	and #$7F		; Clear MSB
+	sta VOLnam,x		; Store character in VOLnam
+	inx
+	cpx #$0F		; Have 15 characters been entered?
+	bcc LInput
+LFormat:
+	txa			; See if default VOLUME_NAME was taken
+	bne LSetLEN
+WLoop:
+	lda MBlank,x		; Transfer 'BLANK' to VOLnam
+	and #$7F		; Clear MSB
+	sta VOLnam,x
+	inx
+	cpx #$05		; End of transfer?
+	bcc WLoop
+	lda #$13		; Reset CH to 19
+	jsr HTAB
+LSetLEN:
+	jsr CLRLN		; Erase the rest of the line
+	clc
+	txa			; Add $F0 to Volume Name length
+	adc #$F0		; Create STORAGE_TYPE, NAME_LENGTH byte
+	sta VolLen
+	jsr ECHO_OFF
+	clc
+	rts
+LAbort:
+	jsr ECHO_OFF
+	sec
+	rts
+
+VolLen:	.res 1			; $F0 + length of Volume Name
+VOLnam:	.res 15			; Volume Name
+
 ; SOS formatter messages
-FMTMsg01: asc	"DRIVE TOO SLOW!"
+FMTMsg01: asc	"DRIVE TOO SLOW! ADJUST CLOCKWISE."
 FMTMsg01_END =*
-FMTMsg02: asc	"DRIVE TOO FAST!"
+FMTMsg02: asc	"DRIVE TOO FAST! ADJUST ANTI-CLOCKWISE."
 FMTMsg02_END =*
 
 FMT_MSG_LEN_TBL:
