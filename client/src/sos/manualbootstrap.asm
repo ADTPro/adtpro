e54572b10c1900328f123b0da410a08ed8e345ba
David Schmidt <david__schmidt@users.sourceforge.net>
2008-09-05 14:18:22 +0000

    Trimming the bootstrapping code down a bit

--- client/src/sos/manualbootstrap.asm
+++ client/src/sos/manualbootstrap.asm
@@ -27,109 +27,107 @@
 	.org $a000
 
 KBDSTROBE	:= $C010
-E_REG		:= $FFDF
-B_REG		:= $FFEF
-buffer		:= $7e
+ENV_REG		:= $FFDF
+BANK_REG	:= $FFEF
 buffer_lsb	:= $7e
 buffer_msb	:= $7f
+
+ACIADR		:= $c0f0	; Data register. $c0f0 for ///, $c088+S0 for SSC
+ACIASR		:= $c0f1	; Status register. $c0f1 for ///, $c089+S0 for SSC
+ACIAMR		:= $c0f2	; Command mode register. $c0f2 for ///, $c08a+S0 for SSC
+ACIACR		:= $c0f3	; Control register.  $c0f3 for ///, $c08b+S0 for SSC
 
 entry:	sei
 	cld
 	lda #$77
-	sta E_REG
+	sta ENV_REG
 	ldx #$FB
 	txs
 	bit KBDSTROBE
 	lda #$40
-	sta $FFCA	; Disable interrupts
+	sta $FFCA		; Disable interrupts
 	lda #$07
-	sta B_REG
-	ldx #$00
-banktest:		; Find highest writable bank
-	dec B_REG
+	sta BANK_REG
+	ldx #$00		; Note - we rely on x remaining zero until we print our welcome message
+banktest:			; Find highest writable bank
+	dec BANK_REG
 	stx $2000
 	lda $2000
 	bne banktest
 
 ; Slow down to 1MHz
-	lda $ffdf	; Read the environment register
-	ora #$80	; Set 1MHz switch
-	sta $ffdf	; Write the environment register
+	lda ENV_REG		; Read the environment register
+	ora #$80		; Set 1MHz switch
+	sta ENV_REG		; Write the environment register
 
 ; Set up our pointers
-	lda #$00
-	tay		; We'll use Y later as an index
-	sta buffer_lsb
-	lda #$1e	; Initial SOS.KERNEL runs from $1e00 to $73ff
+	stx buffer_lsb		; x is still zero
+	lda #$1e		; SOS.KERNEL initially occupies $1e00 to $73ff
 	sta buffer_msb
 
 ; Set up the serial port
-	lda #$0b	; No parity, etc.
-	sta $c0f2
-	lda #$1e	; 9600 bps
-	sta $c0f3
+	lda #$0b		; No parity, etc.
+	sta ACIAMR		; Store via ACIA mode register.
+	lda #$1e		; $16=300, $1e=9600, $1f=19200, $10=115k
+	sta ACIACR		; Store via ACIA control register.
 
-; Say we're active
-	ldx #$00
+; Say we're active		; Note - x is assumed to be zero here
 :	lda message_1,x
 	sta $0400,x
 	inx
 	cpx #$04
 	bne :-
 
-	lda #$57	; "W" - printed after message
-	sta $0405
-
 ; Poll the port until we get a magic incantation
+	ldy #$00
 Poll:
 	jsr IIIGET
-	cmp #$53	; First character will be "S" from "SOS"
+	cmp #$53		; First character will be "S" from "SOS" in SOS.KERNEL
 	bne Poll
-	sta (buffer),y	; Save that first "S"
+	sta (buffer_lsb),y	; Save that first "S"
 	iny
 
 ; We got the magic signature; start reading data
 Read:	
-	jsr IIIGET	; Pull a byte
-	sta (buffer),y	; Save it
-	sta $0405	; Print it in the corner
+	jsr IIIGET		; Pull a byte
+	sta (buffer_lsb),y	; Save it
+	sta $0405		; Print it in the status area
 	iny
 	bne Read
-	inc buffer_msb
-	cmp #$74
-	bne Read
+	inc buffer_msb		; Increment another page
+	lda buffer_msb
+	cmp #$74		; Are we done? (SOS.KERNEL v1.3 is $56 pages long; $1E+$56=$74)
+	bne Read		; If not... go back for more
 
 ; Say we're done
-	ldx #$00
-:	lda message_2,x
-	sta $0400,x
-	inx
-	cpx #$04
+:	lda message_2,y		; Note - y is still zero from the Read loop above
+	sta $0400,y
+	iny
+	cpy #$04
 	bne :-
 
 ; Go fast again
-	lda $ffdf	; Read the environment register
+	lda ENV_REG	; Read the environment register
 	and #$7f	; Set 2MHz switch
-	sta $ffdf	; Write the environment register
+	sta ENV_REG	; Write the environment register
 
 ; Call SOSLDR entry point
-	jmp $1e70	; SOSLDR Entry point
+	jmp $1e70	; SOSLDR v1.3 Entry point
 
 IIIGET:
-	lda $c000
-	lda $c0f1	; Check status bits
+	lda ACIASR	; Check status bits via ACIA status register
 	and #$68
 	cmp #$08
 	bne IIIGET	; Input register empty, loop
-	lda $c0f0	; Get character
+	lda ACIADR	; Get character via ACIA data register
 	rts
 
 message_1:
-	.byte	"сер╨"	; "SER:"
+	.byte	"сер╨"	; "SER: W" (waiting)
 
 message_2:
-	.byte	"ок║ "	; "OK! "
-;	.byte $d7, $c1, $c9, $d4, $c9, $ce, $c7, $ae, $ae, $ae
+	.byte	"ок║"	; "OK! "
+	.byte	$a0
 
 .align	256
-.assert	* = $a100, error, "Code got too big to fit in a block!  C'mon, someone was supposed to be able to type this in!"
+.assert	* = $a100, error, "Code got too big to fit in a block!  C'mon, someone is supposed to be able to type this into the monitor!"
