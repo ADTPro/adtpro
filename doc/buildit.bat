b7810aa486db8931a2d3b9c2c711e95f9d040585
David Schmidt <david__schmidt@users.sourceforge.net>
2008-10-11 20:47:00 +0000

    Update webpage with Apple /// bootstrapping video

--- doc/buildit.bat
+++ doc/buildit.bat
@@ -10,4 +10,5 @@ perl -i.orig -p ..\..\htmlpassthrough.re index.html
 perl -i.orig -p ..\..\htmlpassthrough.re bootstrap.html
 perl -i.orig -p ..\..\htmlpassthrough.re bootstrapaudio.html
 perl -i.orig -p ..\..\htmlpassthrough.re connectionsserial.html
+perl -i.orig -p ..\..\htmlpassthrough.re bootstrap3.html
 cd ..\..
