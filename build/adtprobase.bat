cc395b602b3b21e32cc1a0e001ca8ea7b621b121
David Schmidt <david__schmidt@users.sourceforge.net>
2009-10-02 22:38:31 +0000

    Have base Windows use older rxtx lib for now

--- build/adtprobase.bat
+++ build/adtprobase.bat
@@ -2,15 +2,17 @@
 REM
 REM ADTPro - Windows startup batch file
 REM
+
+SET ADTPRO_HOME=%CD%\
+
 REM You can set two variables here:
 REM   1. %JAVA_HOME% - to pick a particular java to run under
 REM   2. %ADTPRO_HOME% - to say where you installed ADTPro
 REM
-REM e.g. uncomment (remove the "@REM" from in front) and customize
-REM      the following SET statements.  
+REM e.g. uncomment (remove the "REM" from in front of) and
+REM      customize the following SET statements.  
 REM Note: They must have a trailing backslash as in the examples!
-
-SET ADTPRO_HOME=%CD%\
+REM 
 REM SET ADTPRO_HOME=C:\src\workspace\35\adtpro\build\
 REM SET MY_JAVA_HOME=C:\Progra~1\IBM\Java142\bin\
 
@@ -20,6 +22,7 @@ set RXTX_ARCH=%RXTX_PATH%\Windows\i368-mingw32
 IF "%PROCESSOR_ARCHITECTURE%"=="x86" goto add_classpath
 
 :bit64
+REM Assume 64 bit Windows if not x86
 set RXTX_PATH=%RXTX_VERSION%
 set RXTX_ARCH=%RXTX_PATH%\win64
 
