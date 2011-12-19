59b1e6d13395e7dd78c1917a97cd8ffca2cdefd1
David Schmidt <david__schmidt@users.sourceforge.net>
2011-12-19 05:10:58 +0000

    Add cocmmand line option to start up ADTPro with a particular button "pressed"

--- build/adtprobase.sh
+++ build/adtprobase.sh
@@ -55,4 +55,4 @@ export TWEAK1="-Djava.library.path="
 export TWEAK=$TWEAK1$ADTPRO_HOME/$RXTXLIB
 
 cd "$ADTPRO_HOME"/disks
-"$MY_JAVA_HOME"java -Xms256m -Xmx512m "$TWEAK" $ADTPRO_EXTRA_JAVA_PARMS -cp ../lib/%ADTPRO_VERSION%:../"$RXTXLIB"/../RXTXcomm.jar org.adtpro.ADTPro
+"$MY_JAVA_HOME"java -Xms256m -Xmx512m "$TWEAK" $ADTPRO_EXTRA_JAVA_PARMS -cp ../lib/%ADTPRO_VERSION%:../"$RXTXLIB"/../RXTXcomm.jar org.adtpro.ADTPro $*
