504ce59423195c9d9a3fdb67ef16f2a8c277d315
David Schmidt <david__schmidt@users.sourceforge.net>
2012-01-14 22:04:31 +0000

    Package up a prettier .dmg for MacOS users

--- build/mkdmg.sh
+++ build/mkdmg.sh
@@ -26,6 +26,31 @@ hdiutil eject $MY_DISK
 hdid $DEST$TEMP.dmg
 chflags -R nouchg,noschg "$SRC"
 ditto -rsrcFork -v "$SRC" "/Volumes/$VOLUME"
+
+mkdir "/Volumes/$VOLUME/.background"
+cp ../org/adtpro/resources/ADTProMacInstallBackground.png "/Volumes/$VOLUME/.background/background.png"
+
+echo '
+   tell application "Finder"
+     tell disk "'${VOLUME}'"
+           open
+           set current view of container window to icon view
+           set toolbar visible of container window to false
+           set statusbar visible of container window to false
+           set the bounds of container window to {400, 100, 723, 305}
+           set theViewOptions to the icon view options of container window
+           set arrangement of theViewOptions to not arranged
+           set icon size of theViewOptions to 72
+           set background picture of theViewOptions to file ".background:background.png"
+           make new alias file at container window to POSIX file "/Applications" with properties {name:"Applications"}
+           set position of item "'${VOLUME}'" of container window to {83, 101}
+           set position of item "Applications" of container window to {242, 101}
+           update without registering applications
+           delay 5
+     end tell
+   end tell
+' | osascript
+
 hdiutil eject $MY_DISK
 hdiutil convert -format UDCO $DEST$TEMP.dmg -o $DEST$VOLUME.dmg
 hdiutil internet-enable -yes $DEST$VOLUME.dmg
