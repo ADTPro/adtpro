637a592beeba2644a87fc102f3dee758945ac6eb
David Schmidt <david__schmidt@users.sourceforge.net>
2012-09-04 18:52:20 +0000

    Calculate checksums

--- client/src/prodos/serial/drive/drivemain.asm
+++ client/src/prodos/serial/drive/drivemain.asm
@@ -24,8 +24,8 @@
 UTILPTR		= $6
 
 ; Apple constants
-CHR_ESC		= $9b		; ESCAPE KEY
-cout		= $fded		; Output character
+CHR_ESC	= $9b	; ESCAPE KEY
+CHR_A	= $c1	; Character 'A' 
 
 ; PRODOS GLOBAL PAGE VALUES
 DEV2S1	= $BF14 ; POINTER FOR SLOT 2 DRIVE 1 DRIVER
@@ -45,152 +45,247 @@ IOERR	= $27
 NODEV	= $28
 WPERR	= $2B
 
-	.ORG $1800
+; ROM Locations
+RESETC8	= $CFFF
+COUT	= $fded		; Output character
+
+	.ORG	$1800
 
 	jmp init
+
+CHECKSUM:
+	.byte	$00
 fail:
 INITPAS:
 	jsr	msg
-	.byte	"NO SERIAL SLOT.",$00
+	.byte	"NO SERIAL DEVICE FOUND.",$00
 	rts
 
-; INITIALISE DRIVER
+; INITIALIZE DRIVER
 init:
 ; Find a serial device
-	jsr FindSlot	; Sniff out a likely comm slot
-	lda COMMSLOT
-	bmi fail
+	jsr	msg
+	.byte	"VSDRIVE: ",$00
+	jsr 	FindSlot	; Sniff out a likely comm slot
+	lda	COMMSLOT
+	bmi	fail
+	pha
+	jsr	PARMINT
+	jsr	RESETIO
+	jsr	msg
+	.byte	"USING COMM SLOT ",$00
+	pla
+	clc
+	adc	#$B1	; Add '1' to the found comm slot number for reporting
+	jsr	COUT	; Tell 'em which one we're using
 
 ; ADD POINTER TO DRIVER
-	LDA >DRIVER
-	STA DEV2S1+1
-	LDA <DRIVER
-	STA DEV2S1
+	LDA	#<DRIVER
+	STA	DEV2S1
+	LDA	#>DRIVER
+	STA	DEV2S1+1
 ; ADD TO DEVICE LIST
-	INC DEVCNT
-	LDY DEVCNT
-	LDA #$20 ; SLOT 2 DRIVE 1
-	STA DEVLST,Y
+	INC	DEVCNT
+	LDY	DEVCNT
+	LDA	#$20 ; SLOT 2 DRIVE 1
+	STA	DEVLST,Y
 	RTS
 
 ; DRIVER CODE
 DRIVER:
+	cld
 ; CHECK THAT WE HAVE THE RIGHT DRIVE
-	LDA UNIT
-	CMP #$20 ; SLOT 2 DRIVE 1
-	BEQ DOCMD ; YEP, DO COMMAND
-	SEC ; NOPE, FAIL
-	LDA #NODEV
+	LDA	UNIT
+	CMP	#$20 ; SLOT 2 DRIVE 1
+	BEQ	DOCMD ; YEP, DO COMMAND
+	SEC	; NOPE, FAIL
+	LDA	#NODEV
 	RTS
 
 ; CHECK WHICH COMMAND IS REQUESTED
 DOCMD:
-	LDA COMMAND
-	BNE NOTSTAT ; 0 IS STATUS
-	JMP GETSTAT
+	LDA	COMMAND
+	BNE	NOTSTAT ; 0 IS STATUS
+	JMP	GETSTAT
 NOTSTAT:
-	CMP #$01
-	BNE NOREAD ; 1 IS READ
-	JMP READBLK
+	CMP	#$01
+	BNE	NOREAD ; 1 IS READ
+	JMP	READBLK
 NOREAD:
-	CMP #$02
-	BNE NOWRITE ; 2 IS WRITE
-	JMP WRITEBLK
+	CMP	#$02
+	BNE	NOWRITE ; 2 IS WRITE
+	JMP	WRITEBLK
 NOWRITE:
-	LDA #$00 ; CLEAR ERROR
+	LDA	#$00 ; CLEAR ERROR
 	CLC
 	RTS
-
 
 ; STATUS
 GETSTAT:
-	LDA #$00
-	LDX #$FF
-	LDY #$FF
+	LDA	#$00
+	LDX	#$FF
+	LDY	#$FF
 	CLC
 	RTS
+
+READFAIL:
+	; increment failure count
+	; retry if not too bad
+	sec
+	rts
 
 ; READ
 READBLK:
 ; SEND COMMAND TO PC
-	LDA #$01	; READ COMMAND
-	JSR PUTC
-	LDA BLKLO
-	JSR PUTC
-	LDA BLKHI
-	JSR PUTC
-	LDA #$00	; CHECKSUM
-	JSR PUTC
+	lda	#$01		; Read command
+	jsr	COMMAND_ENVELOPE
 ; READ ECHO'D COMMAND AND VERIFY
-	JSR GETC
-	JSR GETC
-	JSR GETC
-	JSR GETC
+	jsr	GETC		; Command envelope begin
+	cmp	#CHR_A
+	bne	READFAIL
+	jsr	GETC		; Read command
+	cmp	#$01
+	bne	READFAIL
+	jsr	GETC		; LSB of requested block
+	cmp	BLKLO
+	bne	READFAIL
+	jsr	GETC		; MSB of requested block
+	cmp	BLKHI
+	bne	READFAIL
+	jsr	GETC		; Checksum of command envelope
+	cmp	CHECKSUM
+	bne	READFAIL
 
 ; READ BLOCK AND VERIFY
-	LDX #$00
+	LDX	#$00
 RDBKLOOP:
-	LDY #$00
+	LDY	#$00
 RDLOOP:
-	JSR GETC
-	STA (BUFLO),Y
+	jsr	GETC
+	STA	(BUFLO),Y
 	INY
-	BNE RDLOOP
+	BNE	RDLOOP
 
-	INC BUFHI
+	INC	BUFHI
 	INX
-	CPX #$02
-	BNE RDBKLOOP
+	CPX	#$02
+	BNE	RDBKLOOP
 
-	DEC BUFHI
+	DEC	BUFHI
 
-	JSR GETC	; Checksum
-
-	LDA #$00
+	jsr	GETC	; Checksum
+	pha		; Push checksum for now
+	jsr	CALC_CHECKSUM
+	pla	
+	cmp	CHECKSUM
+	bne	READFAIL
+	LDA	#$00
 	CLC
 	RTS
+
+WRITEFAIL:
+	; increment failure count
+	; retry if not too bad
+	sec
+	rts
 
 ; WRITE
 WRITEBLK:
 ; SEND COMMAND TO PC
-	LDA #$02	; WRITE COMMAND
-	JSR PUTC
-	LDA BLKLO
-	JSR PUTC
-	LDA BLKHI
-	JSR PUTC
-	LDA #$00 ; CHECKSUM
-	JSR PUTC
+	lda	#$02		; Write command
+	jsr	COMMAND_ENVELOPE
 
 ; WRITE BLOCK AND CHECKSUM
-	LDX #$00
+	LDX	#$00
+	stx	CHECKSUM
 WRBKLOOP:
-	LDY #$00
+	LDY	#$00
 WRLOOP:
-	LDA (BUFLO),Y
-	JSR PUTC
+	LDA	(BUFLO),Y
+	jsr	PUTC
+	eor	CHECKSUM
+	sta	CHECKSUM
 	INY
-	BNE WRLOOP
+	BNE	WRLOOP
 
-	INC BUFHI
+	INC	BUFHI
 	INX
-	CPX #$02
-	BNE WRBKLOOP
+	CPX	#$02
+	BNE	WRBKLOOP
 
-	DEC BUFHI
+	DEC	BUFHI
 
-	LDA #$00	; Checksum
-	JSR PUTC
+	LDA	CHECKSUM	; Checksum
+	jsr	PUTC
 
 ; READ ECHO'D COMMAND AND VERIFY
-	JSR GETC
-	JSR GETC
-	JSR GETC
-	JSR GETC
+	jsr	GETC
+	cmp	#CHR_A		; S/B Command envelope
+	bne	WRITEFAIL
+	jsr	GETC
+	cmp	#$02		; S/B Write
+	bne	WRITEFAIL
+	jsr	GETC		; LSB of requested block
+	cmp	BLKLO
+	bne	WRITEFAIL
+	jsr	GETC		; MSB of requested block
+	cmp	BLKHI
+	bne	WRITEFAIL
+	jsr	GETC		; Checksum of block - not the command envelope
+	cmp	CHECKSUM
+	bne	WRITEFAIL
 
-	LDA #$00
-	CLC
-	RTS
+	lda	#$00
+	clc
+	rts
+
+COMMAND_ENVELOPE:
+		; Send a command envelope (read/write) with the command in the accumulator
+	pha
+	lda	#CHR_A
+	sta	CHECKSUM
+	pla
+	pha
+	eor	CHECKSUM
+	sta	CHECKSUM
+	lda	BLKLO
+	eor	CHECKSUM
+	sta	CHECKSUM
+	lda	BLKHI
+	eor	CHECKSUM
+	sta	CHECKSUM
+
+	lda	#CHR_A	; Envelope
+	jsr	PUTC
+	pla		; Command
+	jsr	PUTC
+	lda	BLKLO
+	jsr	PUTC
+	lda	BLKHI
+	jsr	PUTC
+	lda	CHECKSUM
+	jsr	PUTC
+	
+	rts
+
+CALC_CHECKSUM:	; Calculate the checksum of the block at BLKLO/BLKHI
+	ldx	#$00
+	stx	CHECKSUM
+CC_OUTER_LOOP:
+	ldy	#$00
+CC_INNER_LOOP:
+	lda	(BUFLO),Y
+	eor	CHECKSUM
+	sta	CHECKSUM
+	iny
+	bne	CC_INNER_LOOP	
+	inc	BUFHI
+	inx
+	cpx	#$02
+	bne	CC_OUTER_LOOP
+
+	dec	BUFHI
+	rts
 
 ;***********************************************
 ;
@@ -207,7 +302,7 @@ msg1:	inc	UTILPTR
 :	lda	(UTILPTR),y
 	beq	msgx
 	ora	#%10000000
-	jsr	cout
+	jsr	COUT
 	jmp	msg1
 msgx:	lda	UTILPTR+1
 	pha
@@ -218,18 +313,18 @@ msgx:	lda	UTILPTR+1
 ;---------------------------------------------------------
 ; PUTC - SEND ACC OVER THE SERIAL LINE (AXY UNCHANGED)
 ;---------------------------------------------------------
-PUTC:	jmp $0000	; Pseudo-indirect JSR - self-modified
+PUTC:	jmp	$0000	; Pseudo-indirect JSR - self-modified
 
 ;---------------------------------------------------------
 ; GETC - GET A CHARACTER FROM SERIAL LINE (XY UNCHANGED)
 ;---------------------------------------------------------
-GETC:	jmp $0000	; Pseudo-indirect JSR - self-modified
+GETC:	jmp	$0000	; Pseudo-indirect JSR - self-modified
 
 ;---------------------------------------------------------
 ; RESETIO - clean up the I/O device
 ;---------------------------------------------------------
 RESETIO:
-	jsr $0000	; Pseudo-indirect JSR to reset the I/O device
+	jsr	$0000	; Pseudo-indirect JSR to reset the I/O device
 	rts
 
 ;---------------------------------------------------------
