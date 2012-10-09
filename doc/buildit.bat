2b8c2e52fca7acee333e02d50d24f0e07b68db09
David Schmidt <david__schmidt@users.sourceforge.net>
2012-10-09 23:34:51 +0000

    Updating some web pages

--- doc/buildit.bat
+++ doc/buildit.bat
@@ -12,4 +12,5 @@ perl -i.orig -p ..\..\htmlpassthrough.re bootstrap.html
 perl -i.orig -p ..\..\htmlpassthrough.re bootstrapaudio.html
 perl -i.orig -p ..\..\htmlpassthrough.re connectionsserial.html
 perl -i.orig -p ..\..\htmlpassthrough.re bootstrap3.html
+perl -i.orig -p ..\..\htmlpassthrough.re lc.html
 cd ..\..
