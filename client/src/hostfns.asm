30de8ecc1647751bdcec7bb4ee9af35fb713967e
David Schmidt <david__schmidt@users.sourceforge.net>
2014-04-29 21:17:05 +0000

    Inverse semantics for SOS

--- client/src/hostfns.asm
+++ client/src/hostfns.asm
@@ -211,10 +211,10 @@ NDUPageDown:
 	jmp DIRDISP0
 
 NDUUnInvertCurrentLine:
-	SET_UNINVERSE_SOS
+	SET_INVERSE_SOS
 	jmp NDUInvertGo
 NDUInvertCurrentLine:
-	SET_INVERSE_SOS
+	SET_UNINVERSE_SOS
 NDUInvertGo:
 	ldy COL_SAV
 	ldx #$00
