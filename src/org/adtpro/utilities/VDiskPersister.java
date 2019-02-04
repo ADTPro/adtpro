aa9e6dcd47ef89751cad9471af5f27313d0a1f5a
David Schmidt <1110325+david-schmidt@users.noreply.github.com>
2019-02-04 17:30:55 -0500

    Track changes to virtual disks on the host, reload if necessary

--- src/org/adtpro/utilities/VDiskPersister.java
+++ src/org/adtpro/utilities/VDiskPersister.java
@@ -21,6 +21,11 @@
 package org.adtpro.utilities;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
 import org.adtpro.gui.Gui;
 import com.webcodepro.applecommander.storage.Disk;
 
@@ -28,6 +33,8 @@ public class VDiskPersister
 {
 	private Gui _parent;
 	Disk _disk1 = null, _disk2 = null;
+    Path _disk1_filePath, _disk2_filePath;
+	FileTime _disk1_modifiedTime, _disk2_modifiedTime;
 
 	private static VDiskPersister _theSingleton = null;
 
@@ -42,19 +49,28 @@ public class VDiskPersister
 
 	private void refreshDisks(int whichDisks)
 	{
+		String pathPrefix = _parent.getWorkingDirectory();		
 		if ((whichDisks == 0) || (whichDisks == 1))
 		{
 			try
 			{
-				_disk1 = new Disk(_parent.getWorkingDirectory() + "Virtual.po"); //$NON-NLS-1$
+				_disk1 = new Disk(pathPrefix + "Virtual.po"); //$NON-NLS-1$
+			    _disk1_filePath = Paths.get(pathPrefix + "Virtual.po");
+		        BasicFileAttributes attrs = Files.readAttributes(_disk1_filePath, BasicFileAttributes.class);
+		        FileTime _disk1_modifiedTime = attrs.lastModifiedTime();
+		        Log.println(false,"VDisk 1 modifiedTime : "+_disk1_modifiedTime.toMillis());
 			}
 			catch (IOException e1)
 			{
 				Log.println(false, "Unable to find Virtual.po in current working directory; creating a new one.");
 				try
 				{
-					com.webcodepro.applecommander.ui.ac.createProDisk(_parent.getWorkingDirectory() + "Virtual.po", "VIRTUAL", Disk.APPLE_800KB_DISK);
-					_disk1 = new Disk(_parent.getWorkingDirectory() + "Virtual.po");
+					com.webcodepro.applecommander.ui.ac.createProDisk(pathPrefix + "Virtual.po", "Hosted.VDrive1", Disk.APPLE_800KB_DISK);
+					_disk1 = new Disk(pathPrefix + "Virtual.po");
+				    _disk1_filePath = Paths.get(pathPrefix + "Virtual.po");
+			        BasicFileAttributes attrs = Files.readAttributes(_disk1_filePath, BasicFileAttributes.class);
+			        FileTime _disk1_modifiedTime = attrs.lastModifiedTime();
+			        Log.println(false,"VDisk 1 modifiedTime : "+_disk1_modifiedTime.toMillis());
 				}
 				catch (IOException e)
 				{
@@ -66,15 +82,23 @@ public class VDiskPersister
 		{
 			try
 			{
-				_disk2 = new Disk(_parent.getWorkingDirectory() + "Virtual2.po"); //$NON-NLS-1$
+				_disk2 = new Disk(pathPrefix + "Virtual2.po"); //$NON-NLS-1$
+			    _disk2_filePath = Paths.get(pathPrefix + "Virtual2.po");
+		        BasicFileAttributes attrs = Files.readAttributes(_disk2_filePath, BasicFileAttributes.class);
+		        FileTime _disk2_modifiedTime = attrs.lastModifiedTime();
+		        Log.println(false,"VDisk 2 modifiedTime : "+_disk2_modifiedTime.toMillis());
 			}
 			catch (IOException e1)
 			{
 				Log.println(false, "Unable to find Virtual2.po in current working directory; creating a new one.");
 				try
 				{
-					com.webcodepro.applecommander.ui.ac.createProDisk(_parent.getWorkingDirectory() + "Virtual2.po", "VIRTUAL", Disk.APPLE_800KB_DISK);
-					_disk2 = new Disk(_parent.getWorkingDirectory() + "Virtual2.po"); //$NON-NLS-1$
+					com.webcodepro.applecommander.ui.ac.createProDisk(pathPrefix + "Virtual2.po", "Hosted.VDrive2", Disk.APPLE_800KB_DISK);
+					_disk2 = new Disk(pathPrefix + "Virtual2.po"); //$NON-NLS-1$
+				    _disk2_filePath = Paths.get(pathPrefix + "Virtual2.po");
+			        BasicFileAttributes attrs = Files.readAttributes(_disk2_filePath, BasicFileAttributes.class);
+			        FileTime _disk2_modifiedTime = attrs.lastModifiedTime();
+			        Log.println(false,"VDisk 2 modifiedTime : "+_disk2_modifiedTime.toMillis());
 				}
 				catch (IOException e)
 				{
@@ -88,10 +112,20 @@ public class VDiskPersister
 	{
 		if (disk == 1)
 		{
+	        BasicFileAttributes attrs = Files.readAttributes(_disk1_filePath, BasicFileAttributes.class);
+	        FileTime new1_modifiedTime = attrs.lastModifiedTime();
+	        Log.println(false,"VDisk 1 modifiedTime : "+new1_modifiedTime.toMillis());
+	        if (_disk1_modifiedTime != new1_modifiedTime)
+	        	refreshDisks(disk); // Disk 1
 			return _disk1.readBlock(block);
 		}
 		else
 		{
+	        BasicFileAttributes attrs = Files.readAttributes(_disk2_filePath, BasicFileAttributes.class);
+	        FileTime new2_modifiedTime = attrs.lastModifiedTime();
+	        Log.println(true,"VDisk 2 modifiedTime : "+new2_modifiedTime.toMillis());
+	        if (_disk2_modifiedTime != new2_modifiedTime)
+	        	refreshDisks(disk); // Disk 2
 			return _disk2.readBlock(block);
 		}
 	}
@@ -102,11 +136,15 @@ public class VDiskPersister
 		{
 			_disk1.writeBlock(block, buffer);
 			_disk1.save();
+	        BasicFileAttributes attrs = Files.readAttributes(_disk1_filePath, BasicFileAttributes.class);
+	        _disk1_modifiedTime = attrs.lastModifiedTime();
 		}
 		else
 		{
 			_disk2.writeBlock(block, buffer);
 			_disk2.save();
+	        BasicFileAttributes attrs = Files.readAttributes(_disk2_filePath, BasicFileAttributes.class);
+	        _disk2_modifiedTime = attrs.lastModifiedTime();
 		}
 	}
 
@@ -121,9 +159,9 @@ public class VDiskPersister
 	}
 
 	/**
-	 * getSingleton() is not synchronized, so we must check in this method to
-	 * make sure a concurrent getSingleton() didn't already allocate the
-	 * Singleton synchronized on a static method locks the class
+	 * getSingleton() is not synchronized, so we must check in this method to make
+	 * sure a concurrent getSingleton() didn't already allocate the Singleton
+	 * synchronized on a static method locks the class
 	 */
 
 	private synchronized static void allocateSingleton(Gui parent)
